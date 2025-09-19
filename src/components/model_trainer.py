f6ca2314780b471a26cf2e90526e9761625158e5
syedaftab-dev <syed.mohd.aftab.2027@gmail.com>
2025-09-19 21:30:40 +0530

    hyperparameter tunining done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,16 +42,44 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                "K-Neighbors Classifier": KNeighborsRegressor(),
+                "KNeighbors Regressor": KNeighborsRegressor(), # <-- Naam sahi karo
                 "XGBRegressor": XGBRegressor(),
                 "CatBoost Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
-
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest":{
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "KNeighbors Regressor":{}, # <-- Isko add karo
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoost Regressor":{ # <-- Naam sahi karo
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
             # evaluate_models->utils will use all models as fit data
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
-            
-            # to get the best model score from dict
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params) # <-- Yahan 'param' likho
+
+            ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
             # convert the keys to list to access values using index and then access value using its index and then get the model name with best_model_score 
