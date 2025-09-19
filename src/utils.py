c6e849a10e95e0416e1ccf5e1442ae5955d4b511
syedaftab-dev <syed.mohd.aftab.2027@gmail.com>
2025-09-19 22:09:32 +0530

    flask app created

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,6 @@
-import os
 import sys
-import pandas as pd
-import numpy as np
+import os
 import dill
-
 from sklearn.metrics import r2_score
 from src.exception import CustomException
 from sklearn.model_selection import GridSearchCV
@@ -47,3 +44,10 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
         
     except Exception as e:
         raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
